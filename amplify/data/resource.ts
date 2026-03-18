b67f47c3d09ffe9ad59c39a194ed9bf0bb1c5678
Tkarsikas <tommi.karsi@gmail.com>
2026-03-18 21:31:05 +0200

    resources updated

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -7,19 +7,33 @@ specifies that any unauthenticated user can "create", "read", "update",
 and "delete" any "Todo" records.
 =========================================================================*/
 const schema = a.schema({
-  Todo: a
-    .model({
-      content: a.string(),
+  BedrockResponse: a.customType({
+    body: a.string(),
+    error: a.string(),
+  }),
+  askBedrock: a
+    .query()
+    .arguments({ ingredients: a.string().array() })
+    .returns(a.ref("BedrockResponse"))
+    .authorization((allow) => [allow.authenticated()])
+    .handler(
+      a.handler.custom({
+        entry: "./bedrock.js",
+        dataSource: "bedrockDS"
     })
-    .authorization((allow) => [allow.guest()]),
+  ),
 });
+
 
 export type Schema = ClientSchema<typeof schema>;
 
 export const data = defineData({
   schema,
   authorizationModes: {
-    defaultAuthorizationMode: 'identityPool',
+    defaultAuthorizationMode: "apiKey",
+    apiKeyAuthorizationMode: {
+      expiresInDays: 30,
+    }, 
   },
 });
 
